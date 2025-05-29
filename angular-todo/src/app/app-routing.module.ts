49259fc0215a24510c2e4ef524ad7777c56cd8f7
veronicamagnanimi <veronica.magnanimi@hotmail.it>
2025-05-29 15:45:26 +0200

    feat: homeComponent & routing implemented

--- angular-todo/src/app/app-routing.module.ts
+++ angular-todo/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent}, //rotta per il componente principale home
+  {path: 'tasks', component: TasksComponent} //rotta per il componente tasks
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular-todo/src/app/app.module.ts
+++ angular-todo/src/app/app.module.ts
@@ -4,13 +4,15 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { TasksComponent } from './tasks/tasks.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    TasksComponent
+    TasksComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
